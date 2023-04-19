fedab9a9f86c8d8883fc4d84734aef95be0cdcf7
keidy9 <keidyw999@gmail.com>
2023-04-18 20:26:40 -0700

    fixed img source

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -40,7 +40,7 @@ const Item = ({ item, width }) => {
           alt={item.name}
           width="300px"
           height="400px"
-          src={`http://localhost:1337${url}`}
+          src={`https://thready.fly.dev${url}`}
           onClick={() => navigate(`/item/${item.id}`)}
           style={{ cursor: "pointer" }}
         />
